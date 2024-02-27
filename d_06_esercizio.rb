0ede378fd0f46ee1ab2fa8ea9e00af0649f344bf
laPhoenixRising <laphoenixrising88@gmail.com>
2024-02-28 00:22:14 +0100

    Esercizio d06

---
+++ d_06_esercizio.rb
@@ -0,0 +1,15 @@
+a = {
+  nome: "Francesca",
+  cognome: "Colombo",
+  età: 35,
+}
+
+a[:film] = [
+  "Il Favoloso Mondo di Amelie",
+  "Titanic",
+  "Carol e la fine del mondo",
+  "The Good Place",
+  "The Crown",
+]
+
+puts a.inspect
