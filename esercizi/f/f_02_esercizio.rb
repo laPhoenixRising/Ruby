fd413567a22b7946325c46cc77bcd8e39d700c36
Francesca Colombo <francesca.colo@gmail.com>
2024-02-28 18:38:57 +0100

    Esercizio f_02

---
+++ esercizi/f/f_02_esercizio.rb
@@ -0,0 +1,11 @@
+def moltiplica(x, y)
+    a = x * y
+    if a > 30
+        puts "#{a} è maggiore di 30"
+    else
+        puts "#{a} non è maggiore di 30"
+    end
+end
+
+moltiplica(10, 8)
+    
