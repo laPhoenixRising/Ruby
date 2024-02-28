9d2b34ed179c87a89d40785d1609ec1557289d42
Francesca Colombo <francesca.colo@gmail.com>
2024-02-28 19:09:01 +0100

    Implementazione esercizio f_03

--- esercizi/f/f_03_esercizio.rb
+++ esercizi/f/f_03_esercizio.rb
@@ -10,6 +10,9 @@ def verifica(x, y)
     if x < 0 && y < 0
         puts "I numeri #{x} #{y} sono negativi"
     end
+    if (x >= 0 && y < 0) || (x < 0 && y >= 0)
+        puts "I numeri sono uno positivo e uno negativo"
+    end 
 end
 
 verifica(-4, -9)
@@ -18,3 +21,4 @@ verifica(5, 0)
 verifica(-3, -3)
 verifica(6, 4)
 verifica(0, 0)
+verifica(-2, 0)
